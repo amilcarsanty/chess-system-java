3bfaf5f0eae112ba14b13eca4dec98efe3b24b25
Amílcar Fernandes <amilcarsanty@gmail.com>
2020-07-13 16:20:33 +0100

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public Program() {
@@ -7,9 +9,7 @@ public class Program {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!!");
-
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 	}
-
 }
