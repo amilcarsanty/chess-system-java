77273666db81db2cab9a0bad06058d1350b6e624
Amílcar Fernandes <amilcarsanty@gmail.com>
2020-07-13 20:27:33 +0100

    ChessException and ChessPosition

--- src/boardgame/BoardException.java
+++ src/boardgame/BoardException.java
@@ -2,17 +2,9 @@ package boardgame;
 
 public class BoardException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
-	private String string;
+
 	
-	public BoardException(String string) {
-		this.string = string;
-	}
-
-	public String getString() {
-		return string;
-	}
-
-	public void setString(String string) {
-		this.string = string;
+	public BoardException(String msg) {
+		super(msg);	
 	}
 }
